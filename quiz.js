e0024cbe8a3f272a6cfbf81dee3e2d2edd80d6c7
Ian Von Fange <wolfgangvonfange@gmail.com>
2021-01-06 14:36:46 -0600

    finished quiz construction function

--- quiz.js
+++ quiz.js
@@ -1,63 +1,74 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-    function Quiz(question, A, B, C, D) {
-        this.question = question;
-        this.answerA = A;
-        this.answerB = B;
-        this.answerC = C;
-        this.answerD = D;
-        this.correct;
+    function Question(q, answerA, answerB, answerC, answerD) {
+        this.question = q;
+        this.A = answerA;
+        this.B = answerB;
+        this.C = answerC;
+        this.D = answerD;
     };
 
-    var questions = [
-        new Quiz("What does HTML stand for?", "JavaScript", "XHTML", "CSS", "Hyper-Text Markup Language", "Hyper-Text Markup Language"),
-        new Quiz("Which language is used for styling web pages?", "HTML", "JQuery", "CSS", "XML"),
-        new Quiz("The condition of an if/else statment is enclosed within:", "Python Script", "Parantheses", "Brackets", "HTML"),
-        new Quiz("What JavaScript syntax can be used to store the output of a function, the content of an array, or a document object?", "let", "const", "var", "All of the above"),
-        new Quiz("What method can be called in a JavaScript file to test JavaScript functions, variables, and objects, all utilizing the browser window?", "console.log()", "getElementById", "Cmnd+A", "list") 
+    const questions = [
+        new Question("What does HTML stand for?", "JavaScript", "Heavy Type Market Letters", "CSS", "Hyper-Text Markup Language"),
+        new Question("Which language is used for styling web pages?", "HTML", "JQuery", "CSS", "XML"),
+        new Question("The condition of an if/else statment is enclosed within:", "Python Script", "Parantheses", "Brackets", "HTML"),
+        new Question("What JavaScript syntax can be used to store the output of a function, the content of an array, or a document object?", "let", "const", "var", "All of the above"),
+        new Question("What method can be called in a JavaScript file to test JavaScript functions, variables, and objects, all utilizing the browser window?", "console.log()", "getElementById", "Cmnd+A", "list")
+    ] 
+    const correctAnswers = [
+        questions[0].answerD,
+        questions[1].answerC,
+        questions[2].answerB,
+        questions[3].answerD,
+        questions[4].answerA    
     ];
-
-    questions[0].correct = questions[0].answerC;
-    questions[1].correct = questions[1].answerC;
-    questions[2].correct = questions[2].answerB;
-    questions[3].correct = questions[3].answerD;
-    questions[4].correct = questions[4].answerA;
-    questions[5].correct = 
-    var score = 0;
-   
-    // function setQuestions(quiz) {
-
-
+    
+    var questionRow = $("<div></div>", {class:"row"}).appendTo($(".container"));
+    var questionDisplay = $("<h4></h4>");
+    var answerRow = $("<div></div>", {class:"row"}).appendTo($(".container"));
+    var answersDisplay = $("<div></div>", {class:"btn-group-vertical"}).appendTo(answerRow);
+    var answerButtonA = $("<button></button>", {class:"btn btn-transparent border-primary", type:"button"});
+    var answerButtonB = $("<button></button>", {class:"btn btn-transparent border-primary", type:"button"});
+    var answerButtonC = $("<button></button>", {class:"btn btn-transparent border-primary", type:"button"});
+    var answerButtonD = $("<button></button>", {class:"btn btn-transparent border-primary", type:"button"}); 
+        
+   function generateQuestions() {
+        $.each(questions, function(){
+            questionDisplay.text(this.question).appendTo(questionRow);
+            answerButtonA.text(this.A).prependTo(answersDisplay);
+            answerButtonB.text(this.B).appendTo(answersDisplay);
+            answerButtonC.text(this.C).appendTo(answersDisplay);
+            answerButtonD.text(this.D).appendTo(answersDisplay);
+        });
+          
+    };
+generateQuestions()
+})
+    
+    // function getUser() {
+    //     var user = prompt("Please enter your name if you would like to save your score, or click Cancel to continue playing as a guest.");
+    //     if (user === "" || user === null) {
+    //         return; 
+    //     } else {
+    //         localStorage.setItem("name" , JSON.stringify(user))
     //     }
-
-    //     // $("#B").val(quiz.answerB);
-    //     // $("#C").val(quiz.answerC);
-    //     // $("#D").val(quiz.answerD);
     // };
-
-    // // function scoreQuestions() {
-    // //     const finalAnswer = true;
-    // //     finalAnswer = $("input").checked;
-    // //     const answer = 
-
-    // // }
-
-    // // function setQuiz(time) {
-    // //     setInterval(function(){
-    // //         time--; }, 1000);
-    // //     time.toString() = $("#clock").text();
-    // //     setQuestions(question1);
-
-    // //     while (time > 0) {
-    // //         for (i=1 ; i < questionsArr.length ; i++) {
-    // //            submitBtn.on("click", setQuestions(questionsArr[i]))
-    // //         }
-    // //     }
+    // 
 
 
 
-    // setQuestions(questions[0])
 
-});
-
-
+// function countDown(){
+//     quizContainer.style.display = "block"
+//     var timerValue = 120
+//     var timeLeft = document.querySelector(".timer")
+//     timeLeft.innerHTML = `<p>${timerValue}</p>`   
+//     var time = setInterval(
+//         function(){
+//             if(timerValue<= 0 || quiz.isEnded(true)){
+//                 clearInterval(time);
+//             }
+//             timeLeft.innerHTML = `<p>${timerValue--}</p>`;
+//          }, 1000);
+//     generateQuestion();
+// } 
